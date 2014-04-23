bcc4b2ea40528f1130ea7f16782723d5a3f83bb0
diogorosa <diogo.m.c.rosa@gmail.com>
2014-04-23 19:48:59 +0100

    Started implementation of server on android client
    Signed-off-by: diogorosa <diogo.m.c.rosa@gmail.com>

--- bomberman.android/src/pt/cmov/bomberman/presenter/GameStartActivity.java
+++ bomberman.android/src/pt/cmov/bomberman/presenter/GameStartActivity.java
@@ -1,20 +1,32 @@
 package pt.cmov.bomberman.presenter;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import pt.cmov.bomberman.R;
-import pt.cmov.bomberman.util.ClientComunicatorTask;
+import pt.cmov.bomberman.util.*;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
 public class GameStartActivity extends ActionBarActivity {
-
+	
+	  private ServerSocket serverSocket;
+      Handler updateConversationHandler;
+      Thread serverThread = null;
+      public static final int SERVERPORT = 6000;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -55,4 +67,34 @@ public class GameStartActivity extends ActionBarActivity {
         String message = "coisas";
         new ClientComunicatorTask().execute(message);
 	}
+	
+    protected void onStop() {
+        super.onStop();
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    class ServerThread implements Runnable {
+        public void run() {
+            Socket socket = null;
+            try {
+                serverSocket = new ServerSocket(SERVERPORT);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    socket = serverSocket.accept();
+                    CommunicationThread commThread = new CommunicationThread(socket);
+                    new Thread(commThread).start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+	
 }
---
+++ bomberman.android/src/pt/cmov/bomberman/util/ServerThread.java
@@ -0,0 +1,5 @@
+package pt.cmov.bomberman.util;
+
+public class ServerThread {
+
+}
