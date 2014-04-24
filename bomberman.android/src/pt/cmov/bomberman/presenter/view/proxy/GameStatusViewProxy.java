aa6ef23120a6f1969e9b159d872f8c3535183e06
João Pinho <jpe.pinho@gmail.com>
2014-04-24 19:35:13 +0100

    GameStatusViewProxy is now the controller of the game state display.

---
+++ bomberman.android/src/pt/cmov/bomberman/presenter/view/proxy/GameStatusViewProxy.java
@@ -0,0 +1,63 @@
+package pt.cmov.bomberman.presenter.view.proxy;
+
+import pt.cmov.bomberman.R;
+import pt.cmov.bomberman.model.GameLevel;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.os.CountDownTimer;
+import android.os.Looper;
+import android.view.View;
+import android.widget.TextView;
+
+public class GameStatusViewProxy {
+
+	private final View gameStatusView;
+	private final TextView txtPlayerName;
+	private final TextView txtPlayerScore;
+	private final TextView txtGameTimeLeft;
+	private final TextView txtNumberOfPlayers;
+	private boolean initialized;
+	private CountDownTimer gameTimer;
+
+	public GameStatusViewProxy(View gameStatusView) {
+		this.gameStatusView = gameStatusView;
+		initialized = false;
+
+		txtPlayerName = (TextView) this.gameStatusView.findViewById(R.id.txtPlayerName);
+		txtPlayerScore = (TextView) this.gameStatusView.findViewById(R.id.txtScore);
+		txtGameTimeLeft = (TextView) this.gameStatusView.findViewById(R.id.txtTime);
+		txtNumberOfPlayers = (TextView) this.gameStatusView.findViewById(R.id.txtNumPlayers);
+	}
+
+	private void init(final GameLevel gameState) {
+		initialized = true;
+		
+		gameStatusView.getHandler().post(new Runnable() {
+			@Override
+			public void run() {
+				
+				txtPlayerName.setText(gameState.getPlayer_name());
+				txtPlayerScore.setText("000");
+				txtGameTimeLeft.setText("100");
+				txtNumberOfPlayers.setText(String.format("%3d", 1));
+				
+				(new CountDownTimer(1000 * 100, 1000) {
+					public void onTick(final long millisUntilFinished) {
+						txtGameTimeLeft.setText("" + ((int) millisUntilFinished / 1000));
+					}
+
+					public void onFinish() {
+						txtGameTimeLeft.setText("000");
+					}
+				}).start();
+			}
+		});
+	}
+
+	public void update(GameLevel gameState) {
+		if (!initialized)
+			init(gameState);
+
+		// TODO
+	}
+}
