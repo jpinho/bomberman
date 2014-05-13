89936d3f9138e0a43d1b2c32da394a0561ba2d0e
diogorosa <diogo.m.c.rosa@gmail.com>
2014-05-14 00:15:37 +0100

    Pears now listen to each other
    misses connection
    
    Signed-off-by: diogorosa <diogo.m.c.rosa@gmail.com>

--- bomberman.android/src/pt/cmov/bomberman/presenter/activity/GameStartActivity.java
+++ bomberman.android/src/pt/cmov/bomberman/presenter/activity/GameStartActivity.java
@@ -5,6 +5,7 @@ import pt.cmov.bomberman.model.GameLevel;
 import pt.cmov.bomberman.util.DebouncedOnClickListener;
 import pt.cmov.bomberman.net.wifidirect.SimWifiP2pBroadcastReceiver;
 import pt.utl.ist.cmov.wifidirect.SimWifiP2pBroadcast;
+import pt.utl.ist.cmov.wifidirect.SimWifiP2pDevice;
 import pt.utl.ist.cmov.wifidirect.SimWifiP2pDeviceList;
 import pt.utl.ist.cmov.wifidirect.SimWifiP2pInfo;
 import pt.utl.ist.cmov.wifidirect.SimWifiP2pManager;
@@ -33,25 +34,25 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-public class GameStartActivity extends Activity implements 
-PeerListListener, GroupInfoListener {
+public class GameStartActivity extends Activity implements PeerListListener,
+		GroupInfoListener {
 
 	private boolean mBound = false;
-    private SimWifiP2pManager mManager = null;
-    private Channel mChannel = null;
-    private Messenger mService = null;
-	
+	private SimWifiP2pManager mManager = null;
+	private Channel mChannel = null;
+	private Messenger mService = null;
+
 	public SimWifiP2pManager getManager() {
 		return mManager;
 	}
-	
+
 	public Channel getChannel() {
 		return mChannel;
 	}
-    
-    
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -65,30 +66,33 @@ PeerListListener, GroupInfoListener {
 
 		setContentView(R.layout.activity_game_start);
 
-		((Button) this.findViewById(R.id.btnNewGame)).setOnClickListener(new DebouncedOnClickListener(1000) {
-			@Override
-			public void onDebouncedClick(View v) {
-				btnHostGame_DebouncedClick(v);
-			}
-		});
-		
-		((Button) this.findViewById(R.id.btnPlayOnline)).setOnClickListener(new DebouncedOnClickListener(1000) {
-			@Override
-			public void onDebouncedClick(View v) {
-				btnPlayOnline_DebouncedClick(v);
-			}
-		});
-		
+		((Button) this.findViewById(R.id.btnNewGame))
+				.setOnClickListener(new DebouncedOnClickListener(1000) {
+					@Override
+					public void onDebouncedClick(View v) {
+						btnHostGame_DebouncedClick(v);
+					}
+				});
+
+		((Button) this.findViewById(R.id.btnPlayOnline))
+				.setOnClickListener(new DebouncedOnClickListener(1000) {
+					@Override
+					public void onDebouncedClick(View v) {
+						btnPlayOnline_DebouncedClick(v);
+					}
+				});
+
 		// initialize the WDSim API
 		SimWifiP2pSocketManager.Init(getApplicationContext());
-		
+
 		// register broadcast receiver
 		IntentFilter filter = new IntentFilter();
 		filter.addAction(SimWifiP2pBroadcast.WIFI_P2P_STATE_CHANGED_ACTION);
 		filter.addAction(SimWifiP2pBroadcast.WIFI_P2P_PEERS_CHANGED_ACTION);
 		filter.addAction(SimWifiP2pBroadcast.WIFI_P2P_NETWORK_MEMBERSHIP_CHANGED_ACTION);
 		filter.addAction(SimWifiP2pBroadcast.WIFI_P2P_GROUP_OWNERSHIP_CHANGED_ACTION);
-		SimWifiP2pBroadcastReceiver receiver = new SimWifiP2pBroadcastReceiver(this);
+		SimWifiP2pBroadcastReceiver receiver = new SimWifiP2pBroadcastReceiver(
+				this);
 		registerReceiver(receiver, filter);
 	}
 
@@ -121,13 +125,15 @@ PeerListListener, GroupInfoListener {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 
-				GameLevel.getInstance().setPlayer_name(input.getText().toString());
+				GameLevel.getInstance().setPlayer_name(
+						input.getText().toString());
 				startActivity(intent);
 			}
 		});
 	}
 
-	private void queryPlayerName(final Intent intent, EditText input, OnClickListener okClickListener) {
+	private void queryPlayerName(final Intent intent, EditText input,
+			OnClickListener okClickListener) {
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		builder.setTitle("Player Name");
 
@@ -137,37 +143,57 @@ PeerListListener, GroupInfoListener {
 		// Set up the buttons
 		builder.setPositiveButton("OK", okClickListener);
 
-		builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				dialog.cancel();
-			}
-		});
+		builder.setNegativeButton("Cancel",
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.cancel();
+					}
+				});
 
 		builder.setCancelable(false);
 		builder.show();
 	}
-	
+
 	/*
 	 * Listeners associated to buttons
 	 */
 
 	public void wifiOnButton(View v) {
-   
-        	Intent intent = new Intent(this.getBaseContext(), SimWifiP2pService.class);
-            bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
-			mBound = true;
 
-			// spawn the chat server background task
-			//new IncommingCommTask().execute();
+		Intent intent = new Intent(this.getBaseContext(),
+				SimWifiP2pService.class);
+		bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+		mBound = true;
 
-			guiUpdateDisconnectedState();
-        }
-	
+		// spawn the chat server background task
+		// new IncommingCommTask().execute();
+
+		guiUpdateDisconnectedState();
+	}
+
+	public void wifiOffButton(View v) {
+
+		if (mBound) {
+			unbindService(mConnection);
+			mBound = false;
+		}
+	}
+
+	public void inGroupList(View v) {
+		if (mBound) {
+			mManager.requestGroupInfo(mChannel,
+					(GroupInfoListener) GameStartActivity.this);
+		} else {
+			Toast.makeText(v.getContext(), "Service not bound",
+					Toast.LENGTH_SHORT).show();
+		}
+
+	}
 
 	private void guiUpdateDisconnectedState() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void enableButton(Button btn, boolean enable) {
@@ -176,7 +202,6 @@ PeerListListener, GroupInfoListener {
 		btn.setVisibility(enable ? View.VISIBLE : View.GONE);
 	}
 
-	
 	private ServiceConnection mConnection = new ServiceConnection() {
 		// callbacks for service binding, passed to bindService()
 
@@ -184,7 +209,8 @@ PeerListListener, GroupInfoListener {
 		public void onServiceConnected(ComponentName className, IBinder service) {
 			mService = new Messenger(service);
 			mManager = new SimWifiP2pManager(mService);
-			mChannel = mManager.initialize(getApplication(), getMainLooper(), null);
+			mChannel = mManager.initialize(getApplication(), getMainLooper(),
+					null);
 			mBound = true;
 		}
 
@@ -196,18 +222,51 @@ PeerListListener, GroupInfoListener {
 			mBound = false;
 		}
 	};
-	
-	
+
 	@Override
 	public void onGroupInfoAvailable(SimWifiP2pDeviceList devices,
 			SimWifiP2pInfo groupInfo) {
-		// TODO Auto-generated method stub
 		
+		// compile list of network members
+		StringBuilder peersStr = new StringBuilder();
+		for (String deviceName : groupInfo.getDevicesInNetwork()) {
+			SimWifiP2pDevice device = devices.getByName(deviceName);
+			String devstr = "" + deviceName + " (" + 
+				((device == null)?"??":device.getVirtIp()) + ")\n";
+			peersStr.append(devstr);
+		}
+
+		// display list of network members
+		new AlertDialog.Builder(this)
+	    .setTitle("Devices in WiFi Network")
+	    .setMessage(peersStr.toString())
+	    .setNeutralButton("Dismiss", new DialogInterface.OnClickListener() {
+	        public void onClick(DialogInterface dialog, int which) { 
+	        }
+	     })
+	     .show();
+
 	}
 
 	@Override
 	public void onPeersAvailable(SimWifiP2pDeviceList peers) {
-		// TODO Auto-generated method stub
+		StringBuilder peersStr = new StringBuilder();
 		
+		// compile list of devices in range
+		for (SimWifiP2pDevice device : peers.getDeviceList()) {
+			String devstr = "" + device.deviceName + " (" + device.getVirtIp() + ")\n";
+			peersStr.append(devstr);
+		}
+
+		// display list of devices in range
+		new AlertDialog.Builder(this)
+	    .setTitle("Devices in WiFi Range")
+	    .setMessage(peersStr.toString())
+	    .setNeutralButton("Dismiss", new DialogInterface.OnClickListener() {
+	        public void onClick(DialogInterface dialog, int which) { 
+	        }
+	     })
+	     .show();
+
 	}
 }
